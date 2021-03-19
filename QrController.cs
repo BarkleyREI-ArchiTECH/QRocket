fe97422e2e2c685fd0e117b2ecc8289c4750649f
Ris Adams <chris@tuesdaybegins.com>
2021-03-19 12:40:00 -0400

    Addind brand options

--- QrController.cs
+++ QrController.cs
@@ -1,6 +1,8 @@
-﻿using System.Drawing.Imaging;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using QRCoder;
 
 namespace QRBuddy
@@ -9,13 +11,24 @@ namespace QRBuddy
     [ApiController]
     public class QrController : ControllerBase
     {
+        private readonly IHostEnvironment _env;
+
+        public QrController(IHostEnvironment env) => _env = env;
+
         [HttpGet("url")]
         public ActionResult Url([FromQuery] string url)
         {
+            var root = _env.ContentRootPath;
+
+            var dark  = ColorTranslator.FromHtml("#131313");
+            var light = ColorTranslator.FromHtml("#e5dfdc");
+
+            var icon = (Bitmap) Image.FromFile(Path.Combine(root, "Barkley_Logo_64.png"));
+
             var       generator = new QRCodeGenerator();
             var       data      = generator.CreateQrCode(new PayloadGenerator.Url(url), QRCodeGenerator.ECCLevel.Q);
             var       qr        = new QRCode(data);
-            using var graphic   = qr.GetGraphic(20);
+            using var graphic   = qr.GetGraphic(20, dark, light, icon);
 
             using var stream = new MemoryStream();
             graphic.Save(stream, ImageFormat.Png);
