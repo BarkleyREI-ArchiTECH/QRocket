195bcae458ac1c11d9f58557bf7a92044877ba2c
Ris Adams <chris@tuesdaybegins.com>
2021-03-19 09:25:03 -0400

    Adding Basic generator

---
+++ QrController.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing.Imaging;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace QRBuddy
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class QrController : ControllerBase
+    {
+        [HttpGet("url")]
+        public ActionResult Url([FromQuery] string url)
+        {
+            var       generator = new QRCodeGenerator();
+            var       data      = generator.CreateQrCode(new PayloadGenerator.Url(url), QRCodeGenerator.ECCLevel.Q);
+            var       qr        = new QRCode(data);
+            using var graphic   = qr.GetGraphic(20);
+
+            using var stream = new MemoryStream();
+            graphic.Save(stream, ImageFormat.Png);
+            var result = stream.ToArray();
+            return File(result, "image/png");
+        }
+    }
+}
